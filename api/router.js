433740afffa77cc6e1fdc7dc89f0e00a9f374730
chiragsastry1996 <chiragsastry@gmail.com>
2019-07-15 22:10:47 +0530

    excel load added

--- api/router.js
+++ api/router.js
@@ -1,24 +1,10 @@
 const express = require("express");
 const router = express();
 const path = require('path')
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-var azure = require('azure-storage');
-var blobService = azure.createBlobService();
-const csv = require('csvtojson')
 const excelToJson =  require('convert-excel-to-json')
 const fs = require('fs')
 const multer  = require('multer')
 const sql = require("mssql");
-
-// const csvWriter = createCsvWriter({
-//     path: csvFilePath,
-//     header: [
-//         {id: 'hello', title: 'hello'},
-//         {id: 'foo', title: 'foo'},
-//         {id: 'baz', title: 'baz'}
-//     ]
-// });
-
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -32,39 +18,7 @@ const storage = multer.diskStorage({
     }
   })
    
-  var upload = multer({ storage: storage })
-
-router.post('/upload', upload.single('file'), async (req, res) => {
-    const file = req.file.originalname;
-    let csvFilePath = `public/uploads/${file}`
-    const jsonArray = await csv().fromFile(csvFilePath);
-   
-    await downloadBlob('reports', 'test.csv')
-    csvFilePath = `public/uploads/out.csv`
-    const jsonArray2 = await csv().fromFile(csvFilePath);
-    const final = jsonArray2.concat(jsonArray)
-    
-    const csvWriter = createCsvWriter({
-        path: 'public/test.csv',
-        header: [
-            {id: 'Start Date', title: 'Start Date'},
-            {id: 'Start Time', title: 'Start Time'},
-        ]
-    });
-
-
-    
-    await csvWriter.writeRecords(final) 
- 
-    
-    try {
-        await uploadLocalFile('reports', `public/test.csv`)
-    } catch (error) {
-        console.log(error);
-    }
-    res.send('ok')
-    
-})
+var upload = multer({ storage: storage })
 
 router.post("/upload/excel", upload.single('excel'), async(req, res) => {
     
@@ -114,62 +68,12 @@ router.post("/upload/excel", upload.single('excel'), async(req, res) => {
     
 })
 
-router.get('/', async(req, res) => {
-    var result = await sql.query(
-        "SELECT * FROM reports"
-      );
-      console.log(result);
-      return res.send(result)
+// router.get('/', async(req, res) => {
+//     var result = await sql.query(
+//         "SELECT * FROM reports"
+//       );
+//       console.log(result);
+//       return res.send(result)
       
-})
-
-// const listBlobs =  (containerName) => {
-//     return new Promise((resolve, reject) => {
-//         blobService.listBlobsSegmented(containerName, null, (err, data) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve({ message: `${data.entries.length} blobs in '${containerName}'`, blobs: data.entries });
-//             }
-//         });
-//     });
-// };
-
-const uploadLocalFile = async (containerName, filePath) => {
-    return new Promise((resolve, reject) => {
-        const fullPath = path.resolve(filePath);
-        const blobName = path.basename(filePath);
-        blobService.createBlockBlobFromLocalFile(containerName, blobName, fullPath, err => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `Local file "${filePath}" is uploaded` });
-            }
-        });
-    });
-};
-
-
-
-const downloadBlob =  async (containerName, blobName) => {
-    // const dowloadFilePath = path.resolve('./' + blobName.replace('.txt', '.downloaded.txt'));
-    return new Promise((resolve, reject) => {
-        blobService.getBlobToText(containerName, blobName, (err, data) => {
-            if (err) {
-                
-            } else {
-                fs.writeFile('public/uploads/out.csv', data, (err, result) => {
-                    if(err){
-                        reject(err);
-                        
-                    }
-                    console.log('ok');
-                    
-                })
-                resolve({ message: `Blob downloaded"`, text: data });
-            }
-        });
-    });
-};
-
+// })
 module.exports = router;
